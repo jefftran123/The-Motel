e801f2655b3f3a2b5987bf968674b1df7d2145da
jefftran123 <duykhoi96tn@gmail.com>
2026-06-09 16:47:43 -0500

    adding running function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine.InputSystem.XR;
 public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
-    private float speed = 5f;
+    private float walkingSpeed = 3f;
+    private float runninggSpeed = 5f;
+    private float currentSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,6 +19,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Moving();
+        Running();
     }
 
     void Moving()
@@ -25,6 +28,18 @@ public class PlayerController : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * currentSpeed * Time.deltaTime);
+    }
+
+    void Running()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = runninggSpeed;
+        }
+        else
+        {
+            currentSpeed = walkingSpeed;
+        }
     }
 }
